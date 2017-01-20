9c297c1086df4b118e144ae276b35e32cd2c95a5
KaiChan <kaichan39@outlook.com>
2017-01-20 23:09:07 +0000

    Kai's Commit

---
+++ Kai/FishAI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAI : MonoBehaviour {
+
+	int speed;
+	int jumpHeight;
+	int fallSpeed;
+
+	//Left is false, Right is true
+	public bool pos;
+	bool jump;
+
+	// Use this for initialization
+	void Start () {
+		jump = false;
+		speed = Random.Range(5, 14);
+		jumpHeight = Random.Range(15, 18);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (transform.position.x);
+		if (pos == true) {
+			transform.Translate (new Vector3 (-speed, 0, 0) * Time.deltaTime);
+			if (transform.position.x < -30) {
+				Destroy (this.gameObject);
+			}
+		} else {
+			transform.Translate (new Vector3 (speed, 0, 0) * Time.deltaTime);
+			if (transform.position.x > 30) {
+				Destroy (this.gameObject);
+			}
+		}
+
+		if (transform.position.x > -5 && transform.position.x < 5) {
+			jump = true;
+		}
+			
+		if (jump == true) {
+			jumpAnimation ();
+		}
+	}
+
+	void jumpAnimation()
+	{
+		transform.Translate (new Vector3 (0, jumpHeight, 0) * Time.deltaTime);
+		jumpHeight -= 1; 
+
+		if (transform.position.y < -5 && jumpHeight < 1) {
+			jump = false;
+		}
+	}
+}
---
+++ Kai/SpawnFish.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFish : MonoBehaviour {
+
+	public Rigidbody2D fishRB;
+	public Rigidbody2D fishObj;
+
+	float startTime;
+	bool spawn;
+	int spawnChance;
+
+	// Use this for initialization
+	void Start () {
+		spawnChance = 50;
+		spawn = false;
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (startTime + 15 < Time.time) 
+		{
+			spawn = true;
+		}
+
+		//There is a one in SpawnChance* chance of a spawn
+		if(Random.Range(0,spawnChance) == 1 && spawn == true)
+		{
+			switch (Random.Range (0, 3)) 
+			{
+				case 1:
+				{
+					fishObj = Instantiate (fishRB, new Vector3 (-30, 0, 0), Quaternion.identity);
+					FishAI fishScript = fishObj.GetComponent<FishAI> ();
+					fishScript.pos = false;
+					break;
+				}
+
+				case 2:
+				{
+					fishObj = Instantiate (fishRB, new Vector3 (30, 0, 0), Quaternion.identity);
+					FishAI fishScript = fishObj.GetComponent<FishAI> ();
+					fishScript.pos = true;
+					break;
+				}
+			}
+		}
+	}
+}
