6f86a96ea3f1a30db9c5ff9f8918d4f6d264c03d
cpwitts <cpwitts@hotmail.com>
2017-01-21 10:05:23 +0000

    Wave physics
    Kinda broken

---
+++ Colm/WaveCalculations.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveCalculations : MonoBehaviour 
+{
+	public bool peak = true;
+	public bool transition = false;
+	public float nextHeight = 0f;
+	public float lastHeight = 0f;
+	public float speed = 3;
+	public float offset = 0f;
+	public float x = 0;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (transform.position.y >= -0.2 && transform.position.y <= 0.2) 
+		{
+			if (!transition) 
+			{
+				transition = true;
+
+				if (peak) 
+				{
+					peak = false;
+				} 
+
+				else 
+				{
+					peak = true;
+				}
+
+				lastHeight = nextHeight;
+				x = 0;
+				calcNewNextHeight ();
+			}
+		} 
+
+		else if (transition) 
+		{
+			transition = false;
+		}
+
+		moveToNextHeight ();
+	}
+
+	void calcNewNextHeight()
+	{
+		if(peak)
+		{
+			nextHeight = Random.Range(3.0f, 4.0f);
+		}
+
+		else
+		{
+			nextHeight = Random.Range(-1.0f, 0f);
+		}
+	}
+
+	void moveToNextHeight()
+	{
+
+		x += 1 * Time.deltaTime;
+		if(!peak)
+		{
+			transform.position = new Vector3(transform.position.x,  ((x-2) * (x - Mathf.Sqrt(Mathf.Abs(nextHeight)))) - nextHeight, 0);
+
+
+
+			//transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
+		}
+
+		else if(peak)
+		{
+
+
+			transform.position = new Vector3(transform.position.x, (-(x-2) * (x - Mathf.Sqrt(Mathf.Abs(nextHeight)))) + nextHeight, 0);
+
+			//transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
+		}
+	}
+}
+
+
---
+++ Colm/WaveCalculationsTake2.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveCalculationsTake2 : MonoBehaviour 
+{
+	public float x = 0;
+	public float roughness = 1;
+	public float speed = 1;
+	public float prevX;
+	public float prevY;
+	public float slope;
+
+	void Start()
+	{
+		prevX = x;
+		prevY = transform.position.y;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if (x >= 2 * Mathf.PI) 
+		{
+			x = 0;
+		}
+
+		if (x == 0) 
+		{
+			calcNewRoughness ();
+		}
+		moveToNextHeight ();
+		calcRotation ();
+	}
+
+	void moveToNextHeight()
+	{
+		prevX = x;
+		prevY = transform.position.y;
+		x += speed * Time.deltaTime;
+		transform.position = new Vector3(transform.position.x,  (roughness * Mathf.Sin(x)) - 1f, 0);
+
+	}
+
+	void calcNewRoughness()
+	{
+		roughness = Random.Range (1.0f, 4.0f);
+	}
+
+	void calcRotation()
+	{
+		slope = (transform.position.y - prevY) / (x - prevX);
+
+		if (slope > 0) 
+		{
+			transform.rotation = new Quaternion (0, 0, Mathf.Atan (slope), 1);
+		} 
+
+		else 
+		{
+			transform.rotation = new Quaternion (0, 0, -Mathf.Atan (slope), 1);
+		}
+	}
+}
+
+
