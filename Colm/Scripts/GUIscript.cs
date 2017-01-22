5b5589092dc9a0e2aebdd0afde6248f0f85a8119
cpwitts <cpwitts@hotmail.com>
2017-01-22 02:01:26 +0000

    I don't remember, lol

---
+++ Colm/Scripts/GUIscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIscript : MonoBehaviour {
+
+	public GUIStyle scoreStyle;
+	float score;
+	int displayedScore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		score += Time.deltaTime;
+		displayedScore = (int)score;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label( new Rect(Screen.width / 1.8f, Screen.height / 1.1f, 200, 200), "Score: " + displayedScore, scoreStyle); 
+	}
+}
---
+++ Colm/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+	public GUIStyle endStyle;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGui()
+	{
+		GUI.Label( new Rect(0,0, 400, 400), "GAME OVER", endStyle); 
+	}
+}
